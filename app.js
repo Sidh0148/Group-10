fd860473bed9794a9ff87b4aa0fbf3b21a98aa06
Aakash <sidh0148@algonquinlive.com>
2025-04-02 16:41:31 -0400

    app changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from environment: ' + process.env.ENVIRONMENT);
+  res.end('say hey to environment: ' + process.env.ENVIRONMENT);
 });
 
 server.listen(port, hostname, () => {
